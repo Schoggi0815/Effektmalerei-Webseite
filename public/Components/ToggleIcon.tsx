0eab56d2e22d91e0e85919241847547a2a242535
Matteo Bossi <matt.bossi@bluewin.ch>
2023-08-24 14:31:09 +0200

    use yarn 1, upgrade mui and create dockerfile

--- public/Components/ToggleIcon.tsx
+++ public/Components/ToggleIcon.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import { Styles } from '@material-ui/styles';
+import {withStyles} from "@mui/styles";
 
 const clipPath = (value: React.ReactNode) => ({
   WebkitClipPath: value,
